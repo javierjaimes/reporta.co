75131c56456bdfe5f10ed3e99b87018e3932fd56
Javier Jaimes <sr.pasajero@lanzacohete.co>
2013-12-03 21:33:07 -0500

    Fixed bad uri

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def index
+    uri = 'redis://redistogo:f036d899ead93963b8e414c21f13154e@koi.redistogo.com:10373/'
+     puts URI.parse(URI.encode(uri.strip))
   end
 
   def what
